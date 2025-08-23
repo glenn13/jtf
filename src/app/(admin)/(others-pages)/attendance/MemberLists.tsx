8a9b2d6f3c30d07a99ece53751ca18bdeac138d7
MixGlobal Glenn <glenn@gsmfeed.com>
2025-08-24 00:42:13 +0400

    ". ". :

--- src/app/(admin)/(others-pages)/attendance/MemberLists.tsx
+++ src/app/(admin)/(others-pages)/attendance/MemberLists.tsx
@@ -287,7 +287,7 @@ const MemberLists = ({ eventId, onHandleClosePreview }: IMemberListsProps) => {
                                                                                     }}
                                                                                 >
                                                                                     <div>
-                                                                                        { item?.name } test
+                                                                                        { item?.name } 
                                                                                     </div>
                                                                                     {
                                                                                         (item?.is_new === true) && (
